9512a8aedabdd0012c30bdc7017439cfc32f56ca
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-02-15 09:46:53 +0530

    [vijay]NoDuplicateEntry

--- AddressBookDay13/Program.cs
+++ AddressBookDay13/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
 
-namespace AddressBookDay13
+namespace AddressBookUsingCollection
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Welcome to Address Book!");
+            Console.WriteLine("Enter Address Book Name");
+            string addressBookName = Console.ReadLine();
+            AddressBookCollection addressBookCollection = new AddressBookCollection();
+            AddressBook addressBook = new AddressBook();
+            addressBookCollection.addressBookDictionary.Add(addressBookName, addressBook);
+            int choice;
+            do
+            {
+                Console.WriteLine("Enter Choice:");
+                Console.WriteLine("1) Display Entries");
+                Console.WriteLine("2) enter new Contact");
+                Console.WriteLine("3) Edit Contact");
+                Console.WriteLine("4) remove Contact");
+                Console.WriteLine("5) Add New Address Book");
+                Console.WriteLine("6) List of all Address Book");
+                Console.WriteLine("7) Exit");
+                choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBookCollection.addressBookDictionary[addressBookName].DisplayNamesInAddresBook();
+                        break;
+                    case 2:
+                        addressBookCollection.addressBookDictionary[addressBookName].AddAddressBookEntry();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter First Name");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name");
+                        string lastName = Console.ReadLine();
+                        addressBookCollection.addressBookDictionary[addressBookName].EditContact(firstName, lastName);
+                        break;
+                    case 4:
+                        Console.WriteLine("Enter First Name");
+                        firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name");
+                        lastName = Console.ReadLine();
+                        addressBookCollection.addressBookDictionary[addressBookName].DeleteContact(firstName, lastName);
+                        break;
+                    case 5:
+                        Console.WriteLine("Enter New Address Book Name");
+                        addressBookName = Console.ReadLine();
+                        addressBookCollection.addressBookDictionary.Add(addressBookName, new AddressBook());
+                        Console.WriteLine($"Address Book {addressBookName} selected!");
+                        break;
+                    case 6:
+                        Console.WriteLine("Listing all Address Books");
+                        foreach (var addressBookEntry in addressBookCollection.addressBookDictionary)
+                        {
+                            Console.WriteLine(addressBookEntry.Key);
+                        }
+                        Console.WriteLine("Select an Address Book");
+                        addressBookName = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Thank you!!!!!!!!!");
+                        break;
+                }
+            } while (choice != 7);
+
+
         }
     }
 }
